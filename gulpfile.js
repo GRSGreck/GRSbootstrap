9acbae694b44799237419753ec1e3cced72a1499
Roman <grechukromansergeevich@gmail.com>
2015-07-24 00:56:34 +0300

    A change in the file gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -263,9 +263,9 @@ gulp.task('fonts', function(){
 	.pipe(size({title: 'Fonts_size_bootstrap'}))
 	.pipe(gulp.dest(path.dist.fonts + 'bootstrap/'))
 
-		// Fonts with fontawesome
+	// Fonts with fontawesome
 	gulp.src(path.bower.fontawesome)
-	.pipe(size({title: 'Fonts_size_bootstrap'}))
+	.pipe(size({title: 'Fonts_size_fontawesome'}))
 	.pipe(gulp.dest(path.dist.fonts))
 	.pipe(reload({stream: true}));
 });
